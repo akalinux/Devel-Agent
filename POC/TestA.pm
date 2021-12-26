7f806029b2cace4a737e4668dccfbba1f0230148
Michael Shipper <Michael.Shipper@Charter.com>
2021-12-26 16:45:05 -0600

    removed bad files

--- POC/TestA.pm
+++
@@ -1,9 +0,0 @@
-package TestA;
-
-use Modern::Perl;
-use Moo;
-
-sub test_a {
-  print "getting here\n";
-}
-1;
--- POC/has.pl
+++
@@ -1,9 +0,0 @@
-#!/opt/apps/perl/perl5240/bin/perl
-
-use Modern::Perl;
-use Data::Dumper;
-
-sub has {
-  print Dumper(\@_);
-}
-has 1,2,3;
